4e75d466f6a874495daff90dea491e721f3897be
herehcoder <herehcoder@users.noreply.github.com>
2026-02-25 02:27:35 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict dN7shWy5lTOqK6swdYP0mjtAFlrMwF9eDAD1NxWXkNXecN6Ijk7RpXEduJOJqUA
+-- \restrict Od5iEwy6HsjGTTElvsW05qXiEQz9rnz9hlpG8U7cuXq2xkmmNCycabog9eZg0Cy
 
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.6
@@ -2226,6 +2226,14 @@ COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "
 
 
 --
+-- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
+\.
+
+
+--
 -- Data for Name: flow_state; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
@@ -3866,6 +3874,6 @@ SELECT pg_catalog.setval('"public"."withdrawal_requests_id_seq"', 8, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict dN7shWy5lTOqK6swdYP0mjtAFlrMwF9eDAD1NxWXkNXecN6Ijk7RpXEduJOJqUA
+-- \unrestrict Od5iEwy6HsjGTTElvsW05qXiEQz9rnz9hlpG8U7cuXq2xkmmNCycabog9eZg0Cy
 
 RESET ALL;
