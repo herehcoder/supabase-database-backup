a4b837b8b1ff6520f3c0e2666ef4bde6a0b8b80e
herehcoder <herehcoder@users.noreply.github.com>
2026-04-01 02:49:13 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict FRXw2cWjOsECTaSz3Bkfc5kPjCUgIF487Om6mzhJH5wQkeVkkpTCuaz91YscK3Y
+-- \restrict LdxFsOAQ8lAGsx0UiWXLALaT1QOlTQFY3ETDyBRNaOr0Kvey4ptYghtzqrlyT67
 
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.6
@@ -3890,6 +3890,6 @@ SELECT pg_catalog.setval('"public"."withdrawal_requests_id_seq"', 8, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict FRXw2cWjOsECTaSz3Bkfc5kPjCUgIF487Om6mzhJH5wQkeVkkpTCuaz91YscK3Y
+-- \unrestrict LdxFsOAQ8lAGsx0UiWXLALaT1QOlTQFY3ETDyBRNaOr0Kvey4ptYghtzqrlyT67
 
 RESET ALL;
